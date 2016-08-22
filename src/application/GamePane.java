d79b175e87a1b28862831232eab1ac21b2816ed7
Possardt <rpossardt@gmail.com>
2016-08-22 08:52:05 -0400

    added score label

--- src/application/GamePane.java
+++ src/application/GamePane.java
@@ -13,13 +13,16 @@ private int timeKeeper;
 private Pane gameScreenLayout;
 private AnimationTimer gameTimer;
 public static boolean isGameStarted;	
-private Label gameLabel, countDown, healthLabel;
-private Turret turret;	
+private Label gameLabel, countDown, healthLabel,scoreLabel, currentScoreLabel;
+private Turret turret;
+private int gameScore = 0;
 private double mouseX,mouseY;
 	public GamePane(Stage window){
 		gameLabel = new Label("Defend the Base!");
 		countDown = new Label("3");
 		healthLabel = new Label("Health:");
+		scoreLabel = new Label("Score:");
+		currentScoreLabel = new Label(Integer.toString(gameScore));
 		gameLabel.setLayoutX(180);
 		gameLabel.setLayoutY(40);
 		countDown.setLayoutX(340);
@@ -27,10 +30,17 @@ private double mouseX,mouseY;
 		healthLabel.setLayoutX(30);
 		healthLabel.setLayoutY(20);
 		healthLabel.setId("healthLabel");
+		scoreLabel.setLayoutX(500);
+		scoreLabel.setLayoutY(20);
+		scoreLabel.setId("scoreLabel");
+		currentScoreLabel.setLayoutX(600);
+		currentScoreLabel.setLayoutY(20);
+		currentScoreLabel.setId("currentScoreLabel");
+		
 		gameScreenLayout = new Pane();
 		turret = new Turret();
 		Rectangle bottom = new Rectangle(0,480,700,20);
-		gameScreenLayout.getChildren().addAll(bottom, gameLabel,countDown,healthLabel);
+		gameScreenLayout.getChildren().addAll(bottom, gameLabel,countDown,healthLabel,scoreLabel,currentScoreLabel);
 		addTurretToGamePane(turret);
 		//turret.barrel.getTransforms().add(new Rotate(230, turret.barrel.getX() - 5,turret.barrel.getY()));
 		gameScreenLayout.addEventHandler(MouseEvent.MOUSE_MOVED, new EventHandler<MouseEvent>(){
@@ -93,6 +103,7 @@ private double mouseX,mouseY;
 						//missile2.bulletMissileCollision(turret.getBulletIterator());
 						//missile3.bulletMissileCollision(turret.getBulletIterator());
 						//missile4.bulletMissileCollision(turret.getBulletIterator());
+						gameScore++;
 					}
 				turret.rotateTurret(mouseX, mouseY);
 				turret.turretRecoil(getGameScreenLayout(),timeKeeper);
@@ -102,7 +113,8 @@ private double mouseX,mouseY;
 					addGameOverTextToScreen();
 					this.stop();
 				}
-				timeKeeper++;
+				timeKeeper++;				
+				updateGameScore();
 			}
 			
 		};
@@ -142,6 +154,11 @@ private double mouseX,mouseY;
 		}
 	}
 	
+	private void updateGameScore(){
+		currentScoreLabel.setText(Integer.toString(gameScore));
+	}
+	
+	
 	private void addGameOverTextToScreen(){
 		Label gameOverLabel = new Label("GAME OVER!");
 		gameOverLabel.setId("gameOverLabel");
@@ -149,6 +166,8 @@ private double mouseX,mouseY;
 		gameOverLabel.setLayoutY(30);
 		gameScreenLayout.getChildren().add(gameOverLabel);
 	}
+	
+	
 	
 	private void addTurretToGamePane(Turret t){
 		gameScreenLayout.getChildren().addAll(t.barrel,t.barrelEnd,t.base,t.baseDome, t.turretHealthBar);
